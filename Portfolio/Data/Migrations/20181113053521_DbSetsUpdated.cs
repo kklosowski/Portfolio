fe9f0bbc1dd7f92dec936cb9c02ec6577d635f08
kklosowski <kamil.klosowski@yahoo.com>
2018-11-13 06:00:22 +0000

    Post adding and dynamic display added, database relationships working

---
+++ Portfolio/Data/Migrations/20181113053521_DbSetsUpdated.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Portfolio.Data.Migrations
+{
+    public partial class DbSetsUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Title = table.Column<string>(nullable: false),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    LongText = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    ShortText = table.Column<string>(maxLength: 300, nullable: true),
+                    AuthorForeignKey = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Posts_AspNetUsers_AuthorForeignKey",
+                        column: x => x.AuthorForeignKey,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Comments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    DateCreated = table.Column<DateTime>(nullable: false),
+                    Text = table.Column<string>(nullable: true),
+                    UserForeignKey = table.Column<string>(nullable: true),
+                    PostForeignKey = table.Column<int>(nullable: true),
+                    CommentsForeignKey = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Comments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_CommentsForeignKey",
+                        column: x => x.CommentsForeignKey,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_Posts_PostForeignKey",
+                        column: x => x.PostForeignKey,
+                        principalTable: "Posts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Comments_AspNetUsers_UserForeignKey",
+                        column: x => x.UserForeignKey,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_CommentsForeignKey",
+                table: "Comments",
+                column: "CommentsForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_PostForeignKey",
+                table: "Comments",
+                column: "PostForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Comments_UserForeignKey",
+                table: "Comments",
+                column: "UserForeignKey");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Posts_AuthorForeignKey",
+                table: "Posts",
+                column: "AuthorForeignKey");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Comments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
